da243be43ae85774a8639e9443c8274907b59491
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-08-02 14:24:08 +0700

    Simple Page

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Name from "./Name.svelte"
+</script>
+
+<style>
+	header{
+		background-color: red;
+		padding: 10px;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	li{
+		display: inline-block
+	}
+
+</style>
+
+<header>
+	<ul>
+		<li>Home</li>
+		<li>Product</li>
+	</ul>
+</header>
+<main>
+	<Name/>
+</main>
+
---
+++ src/Name.svelte
@@ -0,0 +1,14 @@
+<script>
+  // Kode kode Javascript
+</script>
+
+<style>
+  /* untuk menulis CSS */
+  h1{
+    color: blue;
+  }
+</style>
+
+<div>
+  <h1>Sobat Code</h1>
+</div>
